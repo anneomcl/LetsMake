071c90f940d3c401d23405556ecac80e73ed10b5
anneomcl <anneomclaughlin@gmail.com>
2015-08-13 20:56:54 -0500

    Added cube and movement

---
+++ Assets/Move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = transform.position;
+
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			pos.z++;
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			pos.z--;
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			pos.x--;
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			pos.x++;
+		}
+
+		transform.position = pos;
+	}
+}

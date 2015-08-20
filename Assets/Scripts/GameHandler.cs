015cd86d1e3ed0022221c53041d28c612093ba76
Madison Williams <madisonjw@gmail.com>
2015-08-20 01:35:17 -0700

    build 5 and 6

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHandler : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		//So you can quit from the game.
+		if (Input.GetKey (KeyCode.Escape)) {
+			Application.Quit();
+		}
+
+	}
+}

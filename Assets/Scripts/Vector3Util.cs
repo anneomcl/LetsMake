6134e81d46544bbcd77366d8400d757d0664491c
Tiago Nascimento <xtheosirian@gmail.com>
2015-09-05 04:44:45 -0300

    Added MovingPlatform Script and a vector3 utility function

---
+++ Assets/Scripts/Vector3Util.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Vector3Util {
+	public static bool Approx(Vector3 a, Vector3 b, float delta) {
+		return Math.Abs(a.x - b.x) < delta && Math.Abs(a.y - b.y) < delta && Math.Abs(a.z - b.z) < delta;
+	}
+}
